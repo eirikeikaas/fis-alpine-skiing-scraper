5596d8b93dba02ae37059eb8e0867b9689c16c20
Eirik Eikås <eirik.eikas@hyper.no>
2018-10-25 14:02:55 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -70,8 +70,8 @@ for link, raceinfo in race_link_results(FIS_URL):
                 'competitor_id': int(athlete_id),
                 'yob': int(result_cell.cssselect("td")[2].text_content()),
                 'nation': result_cell.cssselect("td")[3].text_content().encode('utf-8'),
-                'time': result_cell.cssselect("td")[4].text_content().encode('utf-8'),
-                'behind': result_cell.cssselect("td")[5].text_content().encode('utf-8'),
+                #'time': result_cell.cssselect("td")[4].text_content().encode('utf-8'),
+                #'behind': result_cell.cssselect("td")[5].text_content().encode('utf-8'),
                 'points': float(result_cell.cssselect("td")[6].text_content())
             }
             print result
